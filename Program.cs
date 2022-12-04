4f3234d4c56a7b24289eecc55a5ad7b96d5f0690
ValerValerich <zvalera1991@mail.ru>
2022-12-04 21:23:00 +0300

    Created base answere with List

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Из имеющегося массива строк сформировать массив строк, длика которых меньше либо равна 3 символам.
+
+Console.Clear();
+
+string[] MassOnlySmallString(string[] ar)
+{
+    List <string> result = new List<string>();
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if (ar[i].Length<=3){
+            result.Add(ar[i]);
+        }
+    }
+    string [] massForOut=result.ToArray<string>();
+    return massForOut;
+}
+
+string[] startMass = new string[4] { "Один", "Два", "Три", "00" };
+string[] resMass = MassOnlySmallString(startMass);
+
+Console.WriteLine(String.Join(", ", resMass));
